e79c63d45c0849b1d656a9e58ae2e3bc0822b09b
Vikas Maurya <123553333+VikasMaurya07@users.noreply.github.com>
2023-05-28 01:03:44 +0530

    for editing in file

---
+++ rep.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void replace(){
+    char replace[20][20];
+    FILE *ou = fopen("ram07.txt", "r");
+    int kk = 0;
+    int zeo = 0;
+    while (1)
+    {
+        kk = fscanf(ou,"%s", replace[zeo]);
+        zeo++;
+        if (kk == EOF)
+        {
+            break;
+        }
+    }
+    fclose(ou);
+    char id[40];
+    fflush(stdin);
+    FILE*mou=fopen("ram07.txt","w");
+    int ln;
+    printf("Enter ln:");
+    scanf("%d",&ln);
+    int k=0;
+    while(1){
+        if(k+1==ln){k++;
+             continue;
+        }
+        fprintf(mou,"%s\n",replace[k]);
+         if(k==zeo){
+            break;
+        }
+        k++;
+       
+
+    }
+    fclose(mou);
+}
+
+int main() {
+    replace();
+    return 0;
+}
